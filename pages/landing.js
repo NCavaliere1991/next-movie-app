7c4420754ba50b9878390012e51865e62cfaee8a
Nick Cavaliere <nickcavaliere1991@gmail.com>
2022-08-17 18:46:32 -0400

    finally works

--- pages/landing.js
+++ pages/landing.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Suspense } from 'react'
 import MovieHeading from '../components/MovieHeading'
 import MovieList from '../components/MovieList'
 import PopularMovieList from '../components/PopularMovieList'
@@ -10,8 +10,8 @@ function Landing() {
   const [movies, setMovies] = useState([])
   const [searchTerm, setSearchTerm] = useState('')
   const [watchlist, setWatchlist] = useState([])
-  const person = supabase.auth.user()
   const router = useRouter()
+  const person = supabase.auth.user()
 
   function searchMovie(searchTerm) {
     const url = `https://api.themoviedb.org/3/search/movie?api_key=645b84c0424790ef23fda061c7c0aa17&query=${searchTerm}`
@@ -26,46 +26,50 @@ function Landing() {
 
   async function getWatchlist() {
     let movies = []
-    const { data, error } = await supabase
-      .from('watchlists')
-      .select('movie_id')
-      .match({ user_id: person.id })
+    const user = await supabase.auth.user()
+    if (user) {
+      const { data, error } = await supabase
+        .from('watchlists')
+        .select('movie_id')
+        .match({ user_id: user.id })
 
-    if (data) {
-      for (const movie of data) {
-        const url = `https://api.themoviedb.org/3/movie/${movie.movie_id}?api_key=645b84c0424790ef23fda061c7c0aa17`
-        await fetch(url)
-          .then((res) => res.json())
-          .then((result) => movies.push(result))
+      if (data) {
+        for (const movie of data) {
+          const url = `https://api.themoviedb.org/3/movie/${movie.movie_id}?api_key=645b84c0424790ef23fda061c7c0aa17`
+          await fetch(url)
+            .then((res) => res.json())
+            .then((result) => movies.push(result))
+        }
+        setWatchlist(movies)
       }
-      setWatchlist(movies)
     }
   }
 
   useEffect(() => {
-    if (person) {
-      const onPageLoad = () => {
-        getWatchlist()
-      }
-      if (document.readyState === 'complete') {
-        onPageLoad()
-      } else {
-        window.addEventListener('load', onPageLoad)
-        // Remove the event listener when component unmounts
-        return () => window.removeEventListener('load', onPageLoad)
-      }
+    const onPageLoad = () => {
+      getWatchlist()
     }
-  }, [])
+    if (document.readyState === 'complete') {
+      onPageLoad()
+    } else {
+      window.addEventListener('load', onPageLoad)
+      // Remove the event listener when component unmounts
+      return () => window.removeEventListener('load', onPageLoad)
+    }
+  }, [watchlist])
 
   useEffect(() => {
     searchMovie(searchTerm)
   }, [searchTerm])
 
   async function addToWatchlist(movie) {
-    const { data, error } = await supabase
-      .from('watchlists')
-      .insert({ movie_id: movie.id, user_id: person.id })
-    getWatchlist()
+    const user = await supabase.auth.user()
+    if (user) {
+      const { data, error } = await supabase
+        .from('watchlists')
+        .insert({ movie_id: movie.id, user_id: user.id })
+      getWatchlist()
+    }
   }
 
   async function signOut() {
@@ -84,6 +88,10 @@ function Landing() {
       .delete()
       .match({ movie_id: movie.id })
   }
+
+  useEffect(() => {
+    getWatchlist()
+  })
 
   return (
     <div className="bg-main-blue w-screen h-screen">
